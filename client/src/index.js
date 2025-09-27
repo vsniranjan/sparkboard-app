cd29b9039c8f0013dbbaa4ca5383c976d58d5088
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-27 22:57:23 +0530

    Changes for production

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-import "@fortawesome/fontawesome-free/css/all.css";
 import "./css/style.css";
 import Modal from "./components/Modal";
 import IdeaForm from "./components/IdeaForm";
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "../public"),
@@ -19,6 +19,9 @@ module.exports = {
     hot: true,
     compress: true,
     historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:5000",
+    },
   },
 
   module: {
