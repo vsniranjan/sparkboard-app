d88505779fbed4b83ddc703a0f91c08502e13bd6
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-27 11:13:53 +0530

    added Modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,27 @@
+class Modal {
+  constructor() {
+    this._modal = document.querySelector("#modal");
+    this._modalBtn = document.querySelector("#modal-btn");
+    this.addEventListeners();
+  }
+  open() {
+    this._modal.style.display = "block";
+  }
+
+  close() {
+    this._modal.style.display = "none";
+  }
+
+  outsideClick(event) {
+    if (event.target === this._modal) {
+      this.close();
+    }
+  }
+
+  addEventListeners() {
+    this._modalBtn.addEventListener("click", this.open.bind(this));
+    window.addEventListener("click", this.outsideClick.bind(this));
+  }
+}
+
+export default Modal;
